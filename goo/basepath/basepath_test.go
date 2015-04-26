4616443982d41efedcb9138a35b279c244568e71
Sven Weintuch <sven@weintuch.net>
2015-04-26 17:48:35 +0200

    applied go fmt

--- goo/basepath/basepath_test.go
+++ goo/basepath/basepath_test.go
@@ -1,24 +1,24 @@
 package basepath
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestBasepath(t *testing.T) {
-    base, err := New()
+	base, err := New()
 
-    require.Nil(t, err, "error should be nil")
+	require.Nil(t, err, "error should be nil")
 
-    assert.NotEqual(t, "", base.Path, "Path should not be an empty string")
+	assert.NotEqual(t, "", base.Path, "Path should not be an empty string")
 
-    assert.Equal(t, base.Path, base.GetPath(), "Path and GetPath() should be the same")
+	assert.Equal(t, base.Path, base.GetPath(), "Path and GetPath() should be the same")
 
-    file := "./testfile"
-    withBase := base.Path + "/" + file
-    assert.Equal(t, withBase, base.GetPathWith(file), "GetPathWith() should prepend the base path, when file is relative")
+	file := "./testfile"
+	withBase := base.Path + "/" + file
+	assert.Equal(t, withBase, base.GetPathWith(file), "GetPathWith() should prepend the base path, when file is relative")
 
-    file = "/tmp/file"
-    assert.Equal(t, file, base.GetPathWith(file), "GetPathWith() should return the full file if its an absolute path")
+	file = "/tmp/file"
+	assert.Equal(t, file, base.GetPathWith(file), "GetPathWith() should return the full file if its an absolute path")
 }
