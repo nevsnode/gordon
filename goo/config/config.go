c56f4f24059956efc51801f52cf19904c18df815
Sven Weintuch <sven@weintuch.net>
2015-04-27 12:29:16 +0200

    added comments to config package (issue #7)

--- goo/config/config.go
+++ goo/config/config.go
@@ -1,3 +1,4 @@
+// Package config provides functionality to read and parse the Goophry configuration file.
 package config
 
 import (
@@ -5,22 +6,26 @@ import (
 	"os"
 )
 
+// A Config stores values, necessary for the execution of Goophry.
 type Config struct {
-	RedisNetwork   string
-	RedisAddress   string
-	RedisQueueKey  string
-	Tasks          []Task
-	ErrorCmd       string
-	StatsInterface string
-	Logfile        string
+	RedisNetwork   string // network type used for the connection to Redis
+	RedisAddress   string // network address used for the connection to Redis
+	RedisQueueKey  string // first part of the list-names used in Redis
+	Tasks          []Task // list of available tasks that Goophry can execute
+	ErrorCmd       string // command that will be executed when a taks created an error
+	StatsInterface string // the interface where statistics from Goophry can be gathered from
+	Logfile        string // a file where all output will be written to, instead of stdout
 }
 
+// A Task stores information that task-workers need to execute their script/application.
 type Task struct {
-	Type    string
-	Script  string
-	Workers int
+	Type    string // second part of the list-names used in Redis and used to identify tasks
+	Script  string // path to the script/application that this task should execute
+	Workers int    // number of concurrent go-routines, available for this task
 }
 
+// New reads the provided file and returns a Config instance with the values from it.
+// It may also return an error, when the file doesn't exist, or the content could not be parsed.
 func New(path string) (c Config, err error) {
 	file, err := os.Open(path)
 	if err != nil {
