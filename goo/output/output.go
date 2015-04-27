76ca92e14c3123bdac0002ebcffbd21759e7cc4f
Sven Weintuch <sven@weintuch.net>
2015-04-27 13:28:44 +0200

    added comments to output package (issue #7)

--- goo/output/output.go
+++ goo/output/output.go
@@ -1,3 +1,7 @@
+// Package output handles the output for Goophry.
+// It provides routines to enable debug messages. Also output can be written to a
+// logfile instead of printing it to stdout. Furthermore it provides the possibility
+// to execute a command to notify an external script/application.
 package output
 
 import (
@@ -14,16 +18,21 @@ var (
 	outputCmdErrorOutput = "Error calling ErrorCmd:\n%s\n\nCommand:\n%s"
 )
 
+// outputLogger is an interface implemented by objects that
+// handle the messages from within Goophry.
+// This gives the possibility to provide our own handler for testing purposes.
 type outputLogger interface {
 	Println(...interface{})
 }
 
+// An Output provides routines to handle messages within Goophry.
 type Output struct {
 	debug     bool
 	notifyCmd string
 	logger    outputLogger
 }
 
+// New returns a new instance of Output, writing the messages to stdout per default.
 func New() Output {
 	l := log.New(os.Stdout, "", log.LstdFlags)
 
@@ -32,14 +41,18 @@ func New() Output {
 	}
 }
 
+// SetDebug enables/disables debugging output.
 func (o *Output) SetDebug(d bool) {
 	o.debug = d
 }
 
+// SetNotifyCmd sets the command used for notifying about certain messages.
 func (o *Output) SetNotifyCmd(cmd string) {
 	o.notifyCmd = cmd
 }
 
+// SetLogfile modifies the Output object to write messages to the given logfile instead of stdout.
+// It may return an error, if something went wrong with opening the file.
 func (o *Output) SetLogfile(path string) error {
 	file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
@@ -51,22 +64,28 @@ func (o *Output) SetLogfile(path string) error {
 	return nil
 }
 
+// Debug writes a message to the current output, when debugging output is enabled.
 func (o Output) Debug(msg string) {
 	if o.debug {
 		o.logger.Println(msg)
 	}
 }
 
+// StopError writes a message to the current output, executes the notify-command
+// and exits Goophry with the status 1.
 func (o Output) StopError(msg string) {
 	o.logger.Println(msg)
 	o.notify(msg)
 	os.Exit(1)
 }
 
+// NotifyError executes the notify-command with a given message.
 func (o Output) NotifyError(msg string) {
 	o.notify(msg)
 }
 
+// notify tries to execute the notify-command with the given message.
+// In case of an error it will write the error and message to the current output.
 func (o Output) notify(msg string) {
 	var err error
 	var out []byte
